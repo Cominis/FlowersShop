808b4b3d71755dded98da1ff58dc40c44ac300ee
Skarulis Justinas <jskarulis0@gmail.com>
2022-04-07 20:43:59 +0300

    Created db entities templates

---
+++ Backend/FlowerEShopAPI/FlowerEShopAPI/DB/Entities/Shop.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerEShopAPI.DB.Entities
+{
+    public class Shop
+    {
+        public Shop(string shopId,string name, string description, DateTime createdAt, DateTime updatedAt, string userId)
+        {
+            ShopId = shopId;
+            Name = name;
+            Description = description;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            UserId = userId;
+        }
+
+        [Key]
+        [Column(TypeName = "nvarchar(36)")]
+        public string ShopId { get; set; }
+        
+        [Required]
+        [Column(TypeName ="nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(500)")]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        [Required]
+        [ForeignKey("UserId")]
+        public string UserId { get; set; }
+    }
+}
---
+++ Backend/FlowerEShopAPI/FlowerEShopAPI/DB/Entities/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerEShopAPI.DB.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string shoppingCartId)
+        {
+            ShoppingCartId = shoppingCartId;
+        }
+
+        [Key]
+        public string ShoppingCartId { get; set; }
+
+        public virtual List<Product> Product { get; set; }
+    }
+}
