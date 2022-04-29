43ab118b99d16a0edcb026fe4dbd5609f0ccf4ec
Skarulis Justinas <jskarulis0@gmail.com>
2022-04-29 21:52:34 +0300

    Added new startup class

---
+++ Backend/FlowerEShopAPI/FlowerEShopAPI/Startup.cs
@@ -0,0 +1,98 @@
+﻿using FlowerEShopAPI.BL.Exceptions;
+using FlowerEShopAPI.DAL;
+using FlowerEShopAPI.Repositories;
+using FlowerEShopAPI.Repositories.Helpers;
+using FlowerEShopAPI.Repositories.RepositoryInterfaces;
+using FlowerEShopAPI.Repositories.RepositoryInterfaces.HelpersInterfaces;
+using FlowerEShopAPI.Services;
+using FlowerEShopAPI.Services.Helpers;
+using FlowerEShopAPI.Services.ServiceInterfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NLog;
+using System.Text;
+
+namespace FlowerEShopAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/Configs/nlog.config"));
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //DB
+            services.AddControllers();
+            services.AddDbContext<FlowerShopDBContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
+            services.AddControllersWithViews()
+             .AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            // DI
+            //Repos
+            services.AddScoped<IShopRepository, ShopRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IEnumConverter, EnumConverter>();
+            services.AddScoped(provider => new Lazy<IEnumConverter>(provider.GetService<IEnumConverter>));
+            services.AddScoped<IShoppingCartRepository, ShopingCartRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IHelpers, Helpers>();
+            services.AddScoped(provider => new Lazy<IHelpers>(provider.GetService<IHelpers>));
+
+            //Services
+            services.AddScoped<IValidation, Validation>();
+            services.AddScoped(provider => new Lazy<IValidation>(provider.GetService<IValidation>));
+            services.AddScoped<IShopService, ShopService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IShoppingCartService, ShoppingCartService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ISearchService, SearchService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
