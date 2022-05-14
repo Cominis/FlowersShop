352ff2cd10db3aba924e776bba344254e507a701
DESKTOP-8TVEVLU\Vladi <vladis369@gmail.com>
2022-05-14 15:54:38 +0300

    added sign in, sign up to login directory

--- Frontend/flowers-shop/src/App.js
+++ Frontend/flowers-shop/src/App.js
@@ -2,8 +2,8 @@ import { Container } from "@mui/material";
 import Header from "./header/Header";
 import Homepage from "./homepage/Homepage";
 import Cart from "./cart/Cart";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
+import SignIn from "./login/SignIn";
+import SignUp from "./login/SignUp";
 
 const App = () => {
   return (
