877e0da97edde382acc8e5bd9335eb676c1e399b
karinakr8 <karinakra802@gmail.com>
2022-05-27 17:25:52 +0300

    Changed sorting only by category and title

--- Frontend/flowers-shop/src/shop-items/SortList.js
+++ Frontend/flowers-shop/src/shop-items/SortList.js
@@ -3,70 +3,33 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { Button, Grid } from "@mui/material";
 
-const occasion = [
-    { label: 'Valentine\'s Day first' },
-    { label: 'Birthday first' },
-    { label: 'Mother\'s Day first' },
-    { label: 'Anniversary first' },
-    { label: 'Teacher\'s Day first' },
+const category = [
+    { label: 'A-Z' },
 ]
 
-const type = [
-    { label: 'flowers first' },
-    { label: 'bouquets first' },
-]
-
-const price = [
-    { label: 'to 5 EUR' },
-    { label: 'from 5 to 10 EUR' },
-    { label: 'from 10 to 20 EUR' },
-    { label: 'from 20 to 30 EUR' },
-    { label: 'from 30 to 50 EUR' },
-    { label: 'from 50 EUR' },
-]
-
-const popularity = [
-    { label: 'popular first' },
-    { label: 'unpopular first' },
+const title = [
+    { label: 'A-Z' },
 ]
 
 const SortList = (props) => {
     return (
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={2.8}>
+            <Grid item xs={6}>
                 <Autocomplete
                     disablePortal
-                    id="combo-box-occasion"
-                    options={occasion}
+                    id="combo-box-category"
+                    options={category}
                     sx={{ width: 250 }}
-                    renderInput={(params) => <TextField {...params} label="occasion" />}
+                    renderInput={(params) => <TextField {...params} label="category" />}
                 />
             </Grid>
-            <Grid item xs={2.8}>
+            <Grid item xs={6}>
                 <Autocomplete
                     disablePortal
-                    id="combo-box-type"
-                    options={type}
+                    id="combo-box-title"
+                    options={title}
                     sx={{ width: 250 }}
-                    renderInput={(params) => <TextField {...params} label="type" />}
-                />
-            </Grid>
-            <Grid item xs={2.8}>
-                <Autocomplete
-                    disablePortal
-                    id="combo-box-price"
-                    options={price}
-                    sx={{ width: 250 }}
-                    renderInput={(params) => <TextField {...params} label="price" />}
-                />
-             </Grid>
-             <Grid item xs={2.8}>
-                <Autocomplete
-                    disablePortal
-                    id="combo-box-popularity"
-                    options={popularity}
-                    sx={{ width: 250 }}
-                    renderInput={(params) => <TextField {...params} label="popularity" />}
+                    renderInput={(params) => <TextField {...params} label="title" />}
                 />
             </Grid>
         </Grid>
