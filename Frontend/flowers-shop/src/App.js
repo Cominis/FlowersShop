931c153fff084f56f80117d0fddb27aad8b14027
Daumantas <daumantas.simkus@gmail.com>
2022-05-27 19:29:20 +0300

    Add button functionality

--- Frontend/flowers-shop/src/App.js
+++ Frontend/flowers-shop/src/App.js
@@ -9,21 +9,50 @@ import TestApi from "./TestApi";
 import ShopItems from "./shop-items/ShopItems";
 import PersonalDataPage from "./personalDataPage/PersonalDataPage";
 import MyShopPage from "./personalDataPage/MyShopPage";
+import * as React from "react";
+
+const defaultGlobalState = {
+    isLoggedIn: true
+};
+
+const globalStateContext = React.createContext(defaultGlobalState);
+const dispatchStateContext = React.createContext(undefined);
+
+const GlobalStateProvider = ({ children }) => {
+    const [state, dispatch] = React.useReducer(
+        (state, newValue) => ({ ...state, ...newValue }),
+        defaultGlobalState
+    );
+    return (
+        <globalStateContext.Provider value={state}>
+            <dispatchStateContext.Provider value={dispatch}>
+                {children}
+            </dispatchStateContext.Provider>
+        </globalStateContext.Provider>
+    );
+};
+
+export const useGlobalState = () => [
+    React.useContext(globalStateContext),
+    React.useContext(dispatchStateContext)
+];
 
 const App = () => {
     return (
         <Router>
             <Container>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/signIn" element={<SignIn />} />
-                    <Route path="/signUp" element={<SignUp />} />
-                    <Route path="/user" element={<PersonalDataPage />} />
-                    <Route path="/myShop" element={<MyShopPage />} />
-                    {/* <TestApi /> */}
-                </Routes>
+                <GlobalStateProvider>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/signIn" element={<SignIn />} />
+                        <Route path="/signUp" element={<SignUp />} />
+                        <Route path="/user" element={<PersonalDataPage />} />
+                        <Route path="/myShop" element={<MyShopPage />} />
+                        {/* <TestApi /> */}
+                    </Routes>
+                </GlobalStateProvider>
             </Container>
         </Router>
     );
---
+++ Frontend/flowers-shop/src/shop/Shop.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+const Shop = (props) => {
+    return (
+        <Box>
+            <Typography>Test</Typography>
+        </Box>
+    );
+};
+
+export default Shop;
